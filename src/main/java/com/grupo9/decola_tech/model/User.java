6acc3b26f2bb29f2e60107b7dfb5bf4b139a8b17
AndreLuisGomes <andreluisgomes09@gmail.com>
2025-02-26 15:22:12 -0300

    curso deploy

---
+++ src/main/java/com/grupo9/decola_tech/model/User.java
@@ -0,0 +1,32 @@
+package com.grupo9.decola_tech.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tab_user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user_roles"))
+    @Column(name = "role_id")
+    private List<String> role = new ArrayList<>();
+}
---
+++ src/main/java/com/grupo9/decola_tech/service/UserService.java
@@ -0,0 +1,8 @@
+package com.grupo9.decola_tech.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+}
